a096ec7fa83ceb7c89302a5aa240b7d5dac19f4c
silkmeloni <962290874@qq.com>
2025-10-26 00:20:24 +0800

    查看历史记录功能（其实是主程大人做的）

--- ChenXi_VisualNovel/Source/ChenXi_VisualNovel/Private/VNHistoryWidget.cpp
+++ ChenXi_VisualNovel/Source/ChenXi_VisualNovel/Private/VNHistoryWidget.cpp
@@ -3,3 +3,21 @@
 
 #include "VNHistoryWidget.h"
 
+#include "Components/ScrollBox.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UVNHistoryWidget::OnDisplayHistory(const TArray<FDialogLine>& HistoryLines)
+{
+	HistoryScrollBox->ClearChildren();
+	FString historyText = "";
+	for (const FDialogLine& Line : HistoryLines)
+	{
+		if(!Line.CharacterName.IsEmpty())
+			historyText += Line.CharacterName+": ";  //TODO:之后可以考虑换颜色
+		historyText += Line.DialogueText.ToString();
+		historyText += "\n";
+	}
+	TextHistory->SetText(FText::FromString(historyText));
+	TextHistory->SetVisibility(ESlateVisibility::Visible);
+}
--- ChenXi_VisualNovel/Source/ChenXi_VisualNovel/Public/VNHistoryWidget.h
+++ ChenXi_VisualNovel/Source/ChenXi_VisualNovel/Public/VNHistoryWidget.h
@@ -4,6 +4,8 @@
 #include "CoreMinimal.h"
 #include "UI/VNActivatableWidget.h" // 继承自我们的可激活基类
 #include "VNGameMode.h" // 需要 FDialogLine 结构体
+#include "Components/ScrollBox.h"
+#include "Components/TextBlock.h"
 #include "VNHistoryWidget.generated.h"
 
 /**
@@ -14,13 +16,19 @@ UCLASS(Abstract, Blueprintable)
 class CHENXI_VISUALNOVEL_API UVNHistoryWidget : public UVNActivatableWidget
 {
 	GENERATED_BODY()
-
+protected:
+	// 【蓝图绑定】历史记录内容滚动的容器 (必须在蓝图中创建并命名为 ScrollBox_Content)
+	UPROPERTY(meta = (BindWidget)) 
+	TSoftObjectPtr<UScrollBox> HistoryScrollBox;
+	UPROPERTY(meta = (BindWidget))
+	TSoftObjectPtr<UTextBlock> TextHistory;
 public:
 	/**
 	 * 蓝图实现事件：当需要显示历史记录时调用
 	 * C++ 将在 Widget 被创建时调用此事件，并将数据传递给蓝图
 	 * @param HistoryLines 包含所有历史对话的数组
 	 */
-	UFUNCTION(BlueprintImplementableEvent, Category = "VN|History", meta = (DisplayName = "On Display History"))
+	//UFUNCTION(BlueprintImplementableEvent, Category = "VN|History", meta = (DisplayName = "On Display History"))
+	UFUNCTION(Category = "VN|History", meta = (DisplayName = "On Display History"))
 	void OnDisplayHistory(const TArray<FDialogLine>& HistoryLines);
 };
